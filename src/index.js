e175816fd60b8de1c3affb198f091260e8f140e4
vika.ritter.03@gmail.com <vika.ritter.03@gmail.com>
2024-07-23 14:07:29 +0500

    fead: add function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function toReadable (number) {
-  
+   const ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen','fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+   const dozens = [' ',' ','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+   let result = '';
+
+   if (number < 20) {
+      return ones[number];
+   }
+
+   if (number >= 100) {
+      result += ones[Math.floor(number / 100)] + ' hundred';
+      number %= 100;
+      if(number > 0) {
+         result += ' '
+      }
+   }
+
+   if (number>= 20) {
+      result += dozens[Math.floor(number / 10)] + ' ';
+      number %= 10;
+   }
+
+   if (number > 0) {
+      result += ones[number];
+   }
+
+   return result.trim()
 }
